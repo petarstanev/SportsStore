685a8fcc98f7082c03522fece9f25b6d030daf9d
Petar Stanev <PetarStanev@tcb365.onmicrosoft.com>
2018-11-05 15:44:53 +0000

    Added pagination. Added bootstrap.

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -20,9 +21,21 @@ namespace SportsStore.WebUI.Controllers
         // GET: Product
         public ViewResult List(int page = 1)
         {
-            return View(repository.Products.OrderBy(p => p.ProductID).Skip((page-1)* PageSize).Take(PageSize));
-        }
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
 
-        
+        }
     }
 }
